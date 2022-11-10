45ad367e4e1c82d12a5223f435ca47c9ba0035db
Levy-Naibei <levynaibei2@gmail.com>
2022-11-10 16:51:58 +0300

    refactor(main): api calls changed to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+import BookList from './components/BookList';
+import Search from './components/Search';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
+  const [books, setBooks] = React.useState([]);
 
+  const fetchBooks = async () => {
+    const response = await axios(`${process.env.REACT_APP_URL}?api-key=${process.env.REACT_APP_API_KEY}`);
+    setBooks(response.data.results.books);
+  }
+
+  React.useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return (
+    <div className='container mt-5 text-center'>
+      <h3>NYT list of Best-selling books</h3>
+      <Search books={books} />
+      <BookList books={books} />
     </div>
   );
 }
 
 export default App;
+
