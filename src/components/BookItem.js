e0175a1372410728124bb35ae1a16afa2563f845
Levy-Naibei <levynaibei2@gmail.com>
2022-11-10 14:08:33 +0300

    refactor(main): changed BookItem to function component

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../App.css';
 
-export default class BookItem extends Component {
+const BookItem = (props) => {
+    const { book } = props;
+    const { publisher, description, title, book_image } = book;
 
-    render() {
-
-        const{book} =this.props;
-        const{publisher, description, title, book_image} = book;
-
-        return (
-            <div className = 'book'>
-                <div className="media">
-                    <img src={book_image}  className='mr-3 mt-3' style={{width:'100px'}} alt='book-img'/>
-                    <div className="media-body">
-                        <h5><i>{title}</i></h5>
-                        <p>{publisher}</p>
-                        <p>{description}</p>
-                    </div>
+    return (
+        <div className='book'>
+            <div className="media">
+                <img src={book_image} className='mr-3 mt-3' style={{ width: '100px' }} alt='book-img' />
+                <div className="media-body">
+                    <h5><i>{title}</i></h5>
+                    <p>{publisher}</p>
+                    <p>{description}</p>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default BookItem;
 
