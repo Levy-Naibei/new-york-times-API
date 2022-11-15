e5622f74c77c9356ab84df0b04a12062acba8597
Levy-Naibei <levynaibei2@gmail.com>
2022-11-15 15:24:29 +0300

    refactor(rf-book): edited book publisher and author

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const BookItem = (props) => {
     const { book } = props;
-    const { publisher, description, title, book_image, amazon_product_url } = book;
+    const { publisher, description, title, book_image, amazon_product_url, author } = book;
 
     return (
         <div className='book'>
@@ -11,10 +11,11 @@ const BookItem = (props) => {
                 <img src={book_image} className='mr-3 mt-3' style={{ width: '100px' }} alt='book-img' />
                 <div className="media-body">
                     <h5 className='title'><i>{title}</i></h5>
-                    <p>{publisher}</p>
+                    <p>By <span className="author">{author}</span> </p>
                     <p className="desc">{description.substring(0, 80) + '...'}</p>
                 </div>
             </div>
+            <div>Published by <span className="publisher">{publisher}</span></div>
             <div id="order_link">
                 <a href={amazon_product_url}
                     target='_blank' rel='noopener noreferrer'>
