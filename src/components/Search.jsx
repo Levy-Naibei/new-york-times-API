9577e67be71dbb82678a20ae74fa961b807ed448
Levy-Naibei <levynaibei2@gmail.com>
2022-11-15 13:27:04 +0300

    feat(search): added filter data on input change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
-export const Search = (props) => {
-  return (
-    <div className="search">
-        <input type="text" placeholder="Search by title"></input>
-    </div>
-  )
+export const Search = ({ books }) => {
+    const [book, setBooks] = React.useState({ query: '', matches: [] });
+
+    const handleSearch = (e) => {
+        const searchWord = e.target.value;
+        const results = books.filter(book => {
+            if (searchWord === '') return books;
+            return book.title.toLowerCase().includes(searchWord.toLowerCase());
+        });
+        setBooks({ query: searchWord, matches: results });
+        console.log('filtered books', book.matches);
+    }
+
+    return (
+        <div className="search">
+            <input
+                type="search"
+                placeholder="Search by title"
+                onChange={handleSearch}
+                name={book.query}>
+            </input>
+        </div>
+    )
 }
 
 export default Search;
